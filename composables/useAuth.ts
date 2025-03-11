ab532ee0cccfe35370d58b179ce1bb79cce09553
komkem <komkem.k@kkumail.com>
2025-03-11 15:33:32 +0700

    .

---
+++ composables/useAuth.ts
@@ -0,0 +1,18 @@
+import { useIndexStore } from "@/stores/main";
+import services from "@/services/API.service";
+
+const store = useIndexStore();
+
+export const verify = async () => {
+    await services.getUserInfo()
+    .then((resp: any) => {
+      const data = resp.data.data;
+      console.log(data)
+      store.$state.userId = data.id;
+      store.$state.token = data.token;
+    })
+    .catch((error: any) => {
+       console.error(error);
+     })
+     .finally(() => {});
+}
